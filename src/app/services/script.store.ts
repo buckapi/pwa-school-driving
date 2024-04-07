f0d453cd41b5f87a2b9c7cdeb95fecd596e2e5ad
root <root@localhost.localdomain>
2024-04-07 13:53:31 -0500

    first commit

---
+++ src/app/services/script.store.ts
@@ -0,0 +1,11 @@
+
+interface Scripts {
+    name: string;
+    src: string;
+}  
+export const ScriptStore: Scripts[] = [
+
+  {name: 'vendors',src:'./assets/driving/js/vendors.js'},
+  {name: 'main',src:'./assets/driving/js/main.js'}
+  
+];
